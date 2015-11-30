5afdac5968b07f5b0bf1690c0ab8f033d41f4afa
ssacher <ssacher@student.tgm.ac.at>
2015-11-30 13:55:17 +0100

    CalcVolume erstellt

---
+++ workspace/Teamarbeit/src/sacher/calcVolume.java
@@ -0,0 +1,20 @@
+package sacher;
+
+public class calcVolume {
+
+	public Double quaderVolume(Double l, Double b, Double h) {
+		Double quaderV = l*b*h;
+		return quaderV;
+	}
+	
+	public Double kegelVolume(Double l, Double b, Double h) {
+		Double quaderV = l*b*h;
+		return quaderV;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
