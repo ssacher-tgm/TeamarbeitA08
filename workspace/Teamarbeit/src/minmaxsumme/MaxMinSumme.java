d25bfd203f1d6b265102434a75e0d715bbebe64d
Schleppi <Schleppi@Schleppi-PC>
2015-12-04 14:08:29 +0100

    MaxMinSumme added

--- workspace/Teamarbeit/src/minmaxsumme/MaxMinSumme.java
+++ workspace/Teamarbeit/src/minmaxsumme/MaxMinSumme.java
@@ -1,8 +1,19 @@
 package minmaxsumme;
-
+/**
+ * Klasse zur berechnung des Maximums der Minumums und der Summe
+ * @author Mario Ottomaier
+ * @version 4.12.2015
+ */
 public class MaxMinSumme {
 
 
+	/**
+	 * Berechnet das Maximum
+	 * @param a
+	 * @param b
+	 * @return
+	 * @since 4.12.2015
+	 */
 	public Double Maximum(Double a,Double b){
 		if (a<b){
 	        return b; 
@@ -11,6 +22,13 @@ public class MaxMinSumme {
 	       }
 	}
 	
+	/**
+	 * Berechnet das Minumum
+	 * @param a
+	 * @param b
+	 * @return
+	 * @since 4.12.2015
+	 */
 	public Double Minumum(Double a,Double b){
 		if (a>b){
 	        return b; 
@@ -19,6 +37,13 @@ public class MaxMinSumme {
 	       }
 	}
 
+	/**
+	 * Berechnet die Summe 
+	 * @param a
+	 * @param b
+	 * @return
+	 * @since 4.12.2015
+	 */
 	public Double Summe(Double a, Double b){
 		return a+b;
 	}
